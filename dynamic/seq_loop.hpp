1d6a00e4d01e53df74bde0c95e3e1e619fe09643
willisreid96 <77948816+willisreid96@users.noreply.github.com>
2021-10-26 21:26:41 -0400

    Update seq_loop.hpp

--- dynamic/seq_loop.hpp
+++ dynamic/seq_loop.hpp
@@ -1,17 +1,7 @@
 #ifndef __SEQ_LOOP_H
 #define __SEQ_LOOP_H
 
-#include <functional>
-#include <thread>
-#include <vector>
-
-class SeqLoop {
-
-private:
-    size_t threads;
-    std::vector<std::thread> worker_threads;
-
-    
+class SeqLoop {  
 public:
     /// @brief execute the function f multiple times with different
     /// parameters possibly in parallel
@@ -56,50 +46,7 @@ public:
       }
       after(tls);
     }
-    
-    template<typename TLS>
-    static void f_helper(size_t id_thread, std::function<void(int, TLS&)> f, TLS& tls, size_t part_size, size_t total_n, size_t total_threads, std::function<void(TLS&)> before) {
-      before(tls);  
-      int thread_end = (id_thread + 1) * part_size;
-      if (id_thread == total_threads) {
-        thread_end = total_n;
-      }
-      
-      for (size_t i = id_thread * part_size; i < thread_end; i++) {
-        f(i, tls);
-      }
-    }
-    
-    template<typename TLS>
-    void parfor2 (size_t beg, size_t end, size_t increment,
-          std::function<void(TLS&)> before,
-          std::function<void(int, TLS&)> f,
-          std::function<void(TLS&)> after
-          ) {
-      std::vector<TLS> tlss (threads);
-      
-      size_t total_n = end - beg;
-      size_t part_size = total_n / threads; 
-
-      for (size_t id=0; id < threads; id+= 1) {
-        std::thread new_thread (f_helper<TLS>, id, f, std::ref(tlss[id]), part_size, total_n, threads, before);
-        worker_threads.push_back(std::move(new_thread));
-      }
-      
-      for (auto & t : worker_threads) {
-        if (t.joinable()) {
-            t.join();
-        }
-      }
-      
-      for (TLS tls : tlss) {
-        after(tls);
-      }
-    }
-    
-    void thread_count(int count) { 
-      this -> threads = count;
-    }
 };
+    
 
 #endif
