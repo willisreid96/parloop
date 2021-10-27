b806aba64cbad8a0986fe2fa53670f0c5efe6482
willisreid96 <77948816+willisreid96@users.noreply.github.com>
2021-10-26 20:10:37 -0400

    Update dynamic_sched.cpp

--- dynamic/dynamic_sched.cpp
+++ dynamic/dynamic_sched.cpp
@@ -23,6 +23,152 @@ int main (int argc, char* argv[]) {
     std::cerr<<"usage: "<<argv[0]<<" <functionid> <a> <b> <n> <intensity> <nbthreads> <granularity>"<<std::endl;
     return -1;
   }
-
+ int functionID = std::stoi (argv[i]);
+  int a = std::stoi (argv[2]);
+  int b = std::stoi (argv[3]);
+  int n = std::stoi (argv[4]);
+  int intensity = std::stoi (argv[5]);
+  int nbthreads = std::stoi (argv[6]);
+  int granularity = std::stoi (argv[7]);
+  
+  
+  float ban = 1.0 * (b-a) / n;
+  
+  SeqLoop s1;
+  float sum = 0;
+  
+  auto start = std::chrono::steady_clock::now();
+  std::atomic<int> processPointer = 0;
+  
+  switch(functionID)
+  {
+    case 1 :
+      s1.parfor<float>(0, nbthreads, 1, 
+            [&](float& tls) -> void{ 
+              tls = 0;
+            },
+             [&](int pass, float& tls) -> void{
+               int localProcessPointer = processPointer;
+               processPointer += granularity;
+               
+            while (localProcessPointer < n){
+              int endLoop = localProcessPointer + granularity;
+              
+            for (int i = localProcessPointer; i < endLoop; i++)
+            {
+              float x = a + ((i + 0.5) * ban);
+              //
+              tls += f1(x, intensity);
+              
+            }
+              localProcessPointer = processPonter;
+              processPointer += granularity;
+            }
+         
+         },
+               [&](float tls) -> void{
+                 sum = tls * ban;
+               }
+             );
+      break;
+      case 2:
+      
+       s1.parfor<float>(0, nbthreads, 1, 
+            [&](float& tls) -> void{ 
+              tls = 0;
+            },
+             [&](int pass, float& tls) -> void{
+               int localProcessPointer = processPointer;
+               processPointer += granularity;
+               
+            while (localProcessPointer < n){
+              int endLoop = localProcessPointer + granularity;
+              
+            for (int i = localProcessPointer; i < endLoop; i++)
+            {
+              float x = a + ((i + 0.5) * ban);
+              //
+              tls += f2(x, intensity);
+              
+            }
+              localProcessPointer = processPonter;
+              processPointer += granularity;
+            }
+         
+         },
+               [&](float tls) -> void{
+                 sum = tls * ban;
+               }
+             );
+      break;
+      case 3:
+       s1.parfor<float>(0, nbthreads, 1, 
+            [&](float& tls) -> void{ 
+              tls = 0;
+            },
+             [&](int pass, float& tls) -> void{
+               int localProcessPointer = processPointer;
+               processPointer += granularity;
+               
+            while (localProcessPointer < n){
+              int endLoop = localProcessPointer + granularity;
+              
+            for (int i = localProcessPointer; i < endLoop; i++)
+            {
+              float x = a + ((i + 0.5) * ban);
+              //
+              tls += f3(x, intensity);
+              
+            }
+              localProcessPointer = processPonter;
+              processPointer += granularity;
+            }
+         
+         },
+               [&](float tls) -> void{
+                 sum = tls * ban;
+               }
+             );
+      break;
+      case 4:
+       s1.parfor<float>(0, nbthreads, 1, 
+            [&](float& tls) -> void{ 
+              tls = 0;
+            },
+             [&](int pass, float& tls) -> void{
+               int localProcessPointer = processPointer;
+               processPointer += granularity;
+               
+            while (localProcessPointer < n){
+              int endLoop = localProcessPointer + granularity;
+              
+            for (int i = localProcessPointer; i < endLoop; i++)
+            {
+              float x = a + ((i + 0.5) * ban);
+              //
+              tls += f4(x, intensity);
+              
+            }
+              localProcessPointer = processPonter;
+              processPointer += granularity;
+            }
+         
+         },
+               [&](float tls) -> void{
+                 sum = tls * ban;
+               }
+             );
+      break;
+     default :
+      std::cerr<<"invaid functionID";
+      return -1;
+  }
+  auto end =std::chrono:: steady_clock::now();
+  auto milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+  float elapsed = milliseconds / 1000.0;
+  
+  std::cout << std::to_string(sum) << std::end1
+  std::cerr << std::to_string(elapsed) << std::end1;
+  
   return 0;
 }
